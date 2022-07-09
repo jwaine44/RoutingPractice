90204885670bd5232084d702630ff792f9d27a7c
jwaine44 <jwaine44@gmail.com>
2022-07-09 09:57:10 -0400

    Initialize Repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import {useParams} from 'react-router';
+import {useNavigate} from 'react-router-dom';
 import './App.css';
+import {
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+const Home = () => {
+  return (
+    <h1>Welcome</h1>
+  );
+}
+
+const Page = () => {
+  const {value} = useParams();
+    if(isNaN(value)) {
+      return <h1>The word is {value}</h1>
+    } else {
+        return <h1>The number is {value}</h1>
+    }
+}
+
+const ColorPage = () => {
+  const {value} = useParams();
+  const {txtColor} = useParams();
+  const {bgColor} = useParams();
+    if(isNaN(value)) {
+      return <h1 style={{color: txtColor, backgroundColor: bgColor}}>The word is {value}</h1>
+    } else {
+        return <h1 style={{color: txtColor, backgroundColor: bgColor}}>The number is {value}</h1>
+    }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route exact path = '/' element = {<Home />} />
+        <Route path = '/:value' element = {<Page />} />
+        <Route path = '/:value/:txtColor/:bgColor' element = {<ColorPage />} />
+      </Routes>
     </div>
   );
 }
